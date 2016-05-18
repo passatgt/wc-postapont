16e30db2f41c1f9eed67cb10e2a2b56b0ff53ff1
Peter Viszt <peterviszt@Peters-iMac.local>
2016-05-18 12:50:37 +0200

    v 1.0.2
    Frissített PP api

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-/** 
+/**
  * Plugin Name: WooCommerce PostaPont
  * Plugin URI: http://visztpeter.me
- * Description: PostaPont integráció WooCommerce-hez. 
- * Version: 1.0.1
+ * Description: PostaPont integráció WooCommerce-hez.
+ * Version: 1.0.2
  * Author: Peter Viszt
  * Author URI: http://visztpeter.me
  */
@@ -29,7 +29,7 @@ class WC_PostaPont {
 		self::$plugin_basename = plugin_basename(__FILE__);
 		self::$plugin_url = plugin_dir_url(self::$plugin_basename);
 		self::$plugin_path = trailingslashit(dirname(__FILE__));
-		self::$version = '1.0.1'; 
+		self::$version = '1.0.2'; 
 
 		//Add settings to Settings / Shipping
 		add_filter( 'woocommerce_shipping_settings', array( $this, 'settings' ) );
@@ -54,11 +54,11 @@ class WC_PostaPont {
 		add_action( 'woocommerce_email_after_order_table', array( $this, 'wc_postapont_show_selected_location_admin') ,10,1);
 
     }
-    
+
     //Add CSS & JS
 	public function frontend_css_js() {
 		global $post;
-		
+
 		//Only if we're on the checkout page
 		if(get_the_ID () == get_option ( 'woocommerce_checkout_page_id' , 0 )) {
 			$google_maps_url = 'http://maps.googleapis.com/maps/api/js?sensor=false&language=hu&region=HU';
@@ -68,7 +68,7 @@ class WC_PostaPont {
 			wp_enqueue_script('wc-postapont-google-maps', $google_maps_url,array( 'jquery' ));
 			wp_enqueue_style('wc-postapont-css',wc_postapont::$plugin_url.'postapont.css');
 			wp_register_script( 'wc-postapont-js', wc_postapont::$plugin_url.'postapont.js',array( 'jquery' ));
-			
+
 			$categories = array('10_posta','20_molkut','30_csomagautomata');
 			$posta_pont_options = array();
 			foreach($categories as $category) {
@@ -78,7 +78,7 @@ class WC_PostaPont {
 			}
 			wp_localize_script( 'wc-postapont-js', 'posta_pont_options', $posta_pont_options );
 			wp_enqueue_script('wc-postapont-js');
-		
+
 		}
     }
 
@@ -89,7 +89,7 @@ class WC_PostaPont {
 			if ( isset( $section['id'] ) && 'shipping_options' == $section['id'] && isset( $section['type'] ) && 'sectionend' == $section['type'] ) {
 				$shipping_methods = array();
 				global $woocommerce;
-				
+
 				$updated_settings[] = array(
 					'title'         => __( 'Posta Pont Jelölőkategóriák', 'wc_postapont' ),
 					'desc'          => __( 'PostaPontok', 'wc_postapont' ),
@@ -98,7 +98,7 @@ class WC_PostaPont {
 					'type'          => 'checkbox',
 					'checkboxgroup' => 'start'
 				);
-				
+
 				$updated_settings[] = array(
 					'desc'          => __( 'MOL PostaPontok', 'wc_postapont' ),
 					'id'            => 'woocommerce_postapont_20_molkut',
@@ -106,9 +106,9 @@ class WC_PostaPont {
 					'type'          => 'checkbox',
 					'checkboxgroup' => '',
 					'autoload'      => false
-				
+
 				);
-				
+
 				$updated_settings[] = array(
 					'desc'          => __( 'Csomagautomaták', 'wc_postapont' ),
 					'id'            => 'woocommerce_postapont_30_csomagautomata',
@@ -118,12 +118,12 @@ class WC_PostaPont {
 					'autoload'      => false,
 					'desc_tip'      => __( 'Alapértelmezésben minden jelölőkategória megjelenik a térképen. Egy vagy több kategória is kikapcsolható egyszerre.', 'wc_postapont' ),
 				);
-	
+
 				$shipping_methods[''] = __( 'Válassz egy szállítási módot', 'wc_postapont' );
 				foreach ( WC()->shipping->load_shipping_methods() as $method ) {
 					if($method->enabled == 'yes') $shipping_methods[$method->id] = $method->get_title();
 				}
-	
+
 				$updated_settings[] = array(
 					'name'     => __( 'Posta Pont Szállítási Mód', 'wc_postapont' ),
 					'id'       => 'wc_postapont_shipping_method',
@@ -133,7 +133,7 @@ class WC_PostaPont {
 					'class'	   => 'chosen_select',
 					'desc'     => '<br>'.__( 'Válaszd ki azt a szállítási módot, amikor szeretnéd, hogy a Posta Pont térkép megjelenjen.', 'wc_postapont' ),
 				);
-	
+
 				$updated_settings[] = array(
 					'name'     => __( 'Posta Pont Google Api Key', 'wc_postapont' ),
 					'id'       => 'wc_postapont_maps_api',
@@ -141,7 +141,7 @@ class WC_PostaPont {
 					'desc'     => __( 'Opcionális', 'wc_postapont' ),
 					'desc_tip' =>  __( 'A térkép használata ingyenes, azonban nagyobb forgalom esetén üzleti licence szükséges. Készíthet úgynevezett API kulcsot is a térkép alkalmazásához, amellyel nyomon követheti annak használatát, statisztikákat kérhet, és más hasznos tevékenységet végezhet. Opcionális.', 'wc_postapont' ),
 				);
-	
+
 			}
 			$updated_settings[] = $section;
 		}
@@ -151,7 +151,7 @@ class WC_PostaPont {
 	//Display extra HTML on checkout
 	public function wc_postapont_map_html() {
 		$chosen_methods = WC()->session->get( 'chosen_shipping_methods' );
-		$chosen_shipping = $chosen_methods[0]; 
+		$chosen_shipping = $chosen_methods[0];
 		if(get_option('wc_postapont_shipping_method') == $chosen_methods[0]):
 		?>
 		<tr class="wc_postapont_select_row">
@@ -169,12 +169,12 @@ class WC_PostaPont {
 	public function wc_postapont_map_validation() {
 		//Only required if its a posta pont shipping method
 		$chosen_methods = WC()->session->get( 'chosen_shipping_methods' );
-		$chosen_shipping = $chosen_methods[0]; 
+		$chosen_shipping = $chosen_methods[0];
 		if(get_option('wc_postapont_shipping_method') == $chosen_methods[0]) {
-		
+
 		    if ( ! $_POST['wc_selected_postapont'] )
 	        wc_add_notice( __( 'Kérjük, válasszon <strong>átvevőhelyet</strong> a legördülő listából, vagy a térképen egy PostaPontra kattintva!', 'wc_postapont' ), 'error' );
-	
+
 	    }
 	}
 
@@ -183,7 +183,7 @@ class WC_PostaPont {
 		if ( ! empty( $_POST['wc_selected_postapont'] ) ) {
 			update_post_meta( $order_id, 'wc_selected_postapont', sanitize_text_field( $_POST['wc_selected_postapont'] ) );
 	    }
-	}	
+	}
 
 	//Display on the Thank You page
 	public function wc_postapont_thankyou_page($order_id) {
@@ -200,33 +200,33 @@ class WC_PostaPont {
 			<?php
 		}
 	}
-	
+
 	//Display on admin order page
 	public function wc_postapont_show_selected_location_admin($order){
-	
+
 		//Check if PostaPont was selected
 		$posta_pont_data = get_post_meta($order->id,'wc_selected_postapont',true);
 		if($posta_pont_data) {
 			$posta_pont_data = explode('|', $posta_pont_data);
 			$posta_pont_data = implode('<br>', $posta_pont_data);
 		}
-		
+
 		?>
 		<h4><?php _e('A kiválasztott Posta Pont átvevőhely:','wc_postapont'); ?></h4>
 		<p>
 		<?php echo $posta_pont_data; ?>
 		</p>
 		<?php
-	
+
 	}
-	
+
 }
 
 $GLOBALS['wc_postapont'] = new wc_postapont();
 
 //PostaPont szállítási mód súly alapján
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
- 
+
 	function wc_postapont_shipping_method_init() {
 		if ( ! class_exists( 'WC_PostaPont_Shipping_Method' ) ) {
 			class WC_PostaPont_Shipping_Method extends WC_Shipping_Method {
@@ -242,7 +242,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 					$this->method_description = __( 'Ez egy egyszerű súly alapú szállítási mód. A Posta Pont intergrációhoz nem kötelező használni, mert a térképes választót bármely, már meglévő szállítási módhoz hozzá lehet csatolni a Beállítások / Szállítás menüpontban.', 'wc_postapont' ); // Description shown in admin
 					$this->init();
 				}
- 
+
 				/**
 				 * Init your settings
 				 *
@@ -258,12 +258,12 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 					$this->title 		  = $this->get_option( 'title' );
 					$this->tax_status	  = $this->get_option( 'tax_status' );
 					$this->options 		  = (array) explode( "\n", $this->get_option( 'options' ) );
- 
+
 					// Save settings in admin if you have any defined
 					add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
 				}
- 
- 
+
+
 				function init_form_fields() {
 
 					$this->form_fields = array(
@@ -298,9 +298,9 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 							'placeholder'	=> __( 'Minimum súly | Maximum súly | Ár', 'wc_postapont' )
 						)
 					);
-						
+
 				}
- 
+
 				/**
 				 * calculate_shipping function.
 				 *
@@ -309,56 +309,56 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 				 * @return void
 				 */
 				public function calculate_shipping( $package ) {
-	
+
 					if ( sizeof( $this->options ) > 0) {
-			
+
 						//Get items weight
 						global $woocommerce;
 						$total_weight = $woocommerce->cart->cart_contents_weight;
-						
+
 						//Shipping costs
 						$costs = array();
-			
+
 						// Loop options
 						foreach ( $this->options as $option ) {
-			
+
 							$this_option = array_map( 'trim', explode( WC_DELIMITER, $option ) );
-			
+
 							if ( sizeof( $this_option ) !== 3 ) continue;
-							
+
 							if(($total_weight >= $this_option[0]) && ($total_weight <= $this_option[1])) {
 								$costs[] = $this_option[2];
 							}
 						}
-						
+
 						if(!empty($costs)) {
-	
+
 							//Add shipping rate
 							$rate = array(
 								'id' => $this->id,
 								'label' => $this->title,
 								'cost' => min($costs),
 							);
-							
+
 							// Register the rate
 							$this->add_rate( $rate );
-						
+
 						}
-						
+
 					}
-				
+
 				}
 			}
 		}
 	}
- 
+
 	add_action( 'woocommerce_shipping_init', 'wc_postapont_shipping_method_init' );
- 
+
 	function wc_postapont_add_shipping_method( $methods ) {
 		$methods[] = 'WC_PostaPont_Shipping_Method';
 		return $methods;
 	}
- 
+
 	add_filter( 'woocommerce_shipping_methods', 'wc_postapont_add_shipping_method' );
 }
 
--- postapont.js
+++ postapont.js
@@ -9,13 +9,10 @@ jQuery(document).ready(function($){
 		var zipField = 'billing_postcode';
 	}
 
-
-	
-	
 	ppapi = {
-		APIURL: 'http://www.postapont.hu/ppapi/',
+		APIURL: 'https://www.postapont.hu/ppapi/',
 		map: null,
-		pplist: null,
+		pplist: [],
 		ppmarkerslist: null,
 		onSelect: null,
 		opts: {},
@@ -23,25 +20,26 @@ jQuery(document).ready(function($){
 		infoWindow: {},
 		markers: [],
 		skipgroups: {},
-	
+
 		linkZipField: function(zipfield_id){
 			$('#'+zipfield_id).blur( function(){
 				if( ppapi.zipcode == $(this).val() ) return;
 				ppapi.zipcode = $(this).val();
 				ppapi.initZip(ppapi.zipcode);
 			});
-			$('#'+zipfield_id).keypress( function(e){ 
+			$('#'+zipfield_id).keypress( function(e){
 				var code = e.which;
 				if(code==13){
 					ppapi.zipcode = $(this).val();
 					ppapi.initZip(ppapi.zipcode);
+					$(this).blur();
 					return false;
 				}
 			});
 			ppapi.zipcode = $('#'+zipfield_id).val();
 			ppapi.initZip(ppapi.zipcode);
 		},
-	
+
 		apicall: function(method, data, success){
 			$.ajax({
 				crossDomain: true,
@@ -54,49 +52,89 @@ jQuery(document).ready(function($){
 				}
 			});
 		},
-	
+
 		insertMap: function(field_id, opts){
+
 			ppapi.setOpts(opts);
 			ppapi.apicall('insertMap', null, function(data, textStatus, jqXHR ){
 				$('#'+field_id).html(data);
-	
+
 				$('#pp-select-button').click(function(){
 					var ppselectobj = $('#pp-select-postapont');
 					if( !ppapi.pplist || ppselectobj.get(0).selectedIndex<0) return;
 					ppapi.selectPP(ppapi.pplist[ppselectobj.get(0).selectedIndex]);
 				});
-	
+
+				$('#pp-geoloc-button').click(function(){
+					if (!navigator.geolocation) return;
+					navigator.geolocation.getCurrentPosition( ppapi.initGeolocation, function(){
+						alert('Nem sikerült lekédezni az Ön helyzetét.');
+					});
+				});
+
+				$('#pp-select-postapont').change(function(){
+					var item = ppapi.pplist[$(this).get(0).selectedIndex];
+					ppapi.zoomtoPP(item);
+					var marker = new google.maps.Marker({
+						position: new google.maps.LatLng(item.lat, item.lon),
+						data: item
+					});
+					ppapi.mapOpenMarkerInfo(marker);
+				});
+
 				ppapi.mapInitialize();
 			});
 		},
-	
+
 		initZip: function(zip){
 			if(!zip) return;
-			geocoder = new google.maps.Geocoder();
-			geocoder.geocode( { 'address': zip+', Magyarország', 'region':'HU'}, function(results, status) {
-				if (status == google.maps.GeocoderStatus.OK) {
-					ppapi.zipkrd = results[0].geometry.location;
-					ppapi.map.setCenter( ppapi.zipkrd );
-	
-					var skipgroups = Array();
-					for(i in ppapi.skipgroups) if(ppapi.skipgroups[i]) skipgroups.push(i);
-					ppapi.apicall('listPP', {
-						'zipcode': ppapi.zipcode,
-						'lat': ppapi.zipkrd.lat(),
-						'lng': ppapi.zipkrd.lng(),
-						'group': ppapi.opts.group,
-						'skipgroups': skipgroups.join(',')
-						}, function(data, textStatus, jqXHR ){
-							ppapi.pplist = data;
-							ppapi.displayPP();
-						}
-					);
-				} else {
-					alert('Érvénytelen irányítószám!');
+			if(!ppapi.map) return;
+
+			ppapi.apicall('geozip', {
+				'zipCode': zip,
+				}, function(data, textStatus, jqXHR ){
+
+					if(data.type=='symbolzip'){
+						ppapi.zipkrd = new google.maps.LatLng(data.lat, data.lon),
+						ppapi.map.setCenter( ppapi.zipkrd );
+						ppapi.reloadPP();
+
+					} else if(data.type=='mapzip'){
+						geocoder = new google.maps.Geocoder();
+						geocoder.geocode( { 'address': zip+', Magyarország', 'region':'HU'}, function(results, status) {
+							if (status == google.maps.GeocoderStatus.OK) {
+								ppapi.zipkrd = results[0].geometry.location;
+								ppapi.map.setCenter( ppapi.zipkrd );
+								ppapi.reloadPP();
+							} else {
+								//alert('Geocode was not successful for the following reason: ' + status);
+								alert('Érvénytelen irányítószám!');
+							}
+						});
+
+					} else alert('Érvénytelen irányítószám!');
+
 				}
-			});
+			);
+
 		},
-	
+
+		reloadPP: function(fitBounds){
+			var skipgroups = Array();
+			for(i in ppapi.skipgroups) if(ppapi.skipgroups[i]) skipgroups.push(i);
+			ppapi.apicall('listPP', {
+				'zipcode': ppapi.zipcode,
+				'lat': ppapi.zipkrd.lat(),
+				'lng': ppapi.zipkrd.lng(),
+				'group': ppapi.opts.group,
+				'skipgroups': skipgroups.join(',')
+				}, function(data, textStatus, jqXHR ){
+					ppapi.pplist = data;
+					ppapi.displayPP(fitBounds);
+				}
+			);
+		},
+
 		mapInitialize: function(){
 			google.maps.visualRefresh = true;
 			var mapOptions = {
@@ -104,19 +142,24 @@ jQuery(document).ready(function($){
 				mapTypeControl: true,
 				mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
 				scaleControl: true,
-				zoom: 13,
-				center: new google.maps.LatLng(47.499135, 19.043695),
+				zoom: 7,
+				center: new google.maps.LatLng(47.2, 19.6),
 				mapTypeId: google.maps.MapTypeId.ROADMAP
 			};
 			ppapi.map = new google.maps.Map(document.getElementById('pp-map-canvas'), mapOptions);
+
 			ppapi.infoWindow = new google.maps.InfoWindow({});
-			ppapi.symbols['10_posta'] = new google.maps.MarkerImage('http://www.postapont.hu/static/img/mapsymbol-posta.png',
-					new google.maps.Size(25, 20), new google.maps.Point(0,0), new google.maps.Point(10, 19));
-			ppapi.symbols['20_molkut'] = new google.maps.MarkerImage('http://www.postapont.hu/static/img/mapsymbol-molkut.png',
-					new google.maps.Size(25, 20), new google.maps.Point(0,0), new google.maps.Point(10, 19));
-			ppapi.symbols['30_csomagautomata'] = new google.maps.MarkerImage('http://www.postapont.hu/static/img/mapsymbol-csomagautomata.png',
-					new google.maps.Size(20, 29), new google.maps.Point(0,0), new google.maps.Point(10, 19));
-	
+			ppapi.symbols['10_posta'] = new google.maps.MarkerImage(ppapi.APIURL+'../static/img/ppapi/mapsymbol-posta.png',
+					new google.maps.Size(25, 20), new google.maps.Point(0,0), new google.maps.Point(13, 20));
+			ppapi.symbols['20_molkut'] = new google.maps.MarkerImage(ppapi.APIURL+'../static/img/ppapi/mapsymbol-molkut.png',
+					new google.maps.Size(25, 20), new google.maps.Point(0,0), new google.maps.Point(13, 20));
+			ppapi.symbols['30_csomagautomata'] = new google.maps.MarkerImage(ppapi.APIURL+'../static/img/ppapi/mapsymbol-csomagautomata.png',
+					new google.maps.Size(20, 29), new google.maps.Point(0,0), new google.maps.Point(9, 29));
+			ppapi.symbols['40_postapontplusz'] = new google.maps.MarkerImage(ppapi.APIURL+'../static/img/ppapi/mapsymbol-ppplusz.png',
+					new google.maps.Size(25, 20), new google.maps.Point(0,0), new google.maps.Point(13, 20));
+			ppapi.symbols['50_coop'] = new google.maps.MarkerImage(ppapi.APIURL+'../static/img/ppapi/mapsymbol-coop.png',
+					new google.maps.Size(25, 9), new google.maps.Point(0,0), new google.maps.Point(13, 6));
+
 			var skipgroups = Array();
 			for(i in ppapi.skipgroups) if(ppapi.skipgroups[i]) skipgroups.push(i);
 			ppapi.apicall('listPPMarkers', {
@@ -127,21 +170,26 @@ jQuery(document).ready(function($){
 					ppapi.displayPPMarkers();
 				}
 			);
+
+			ppapi.zipkrd = ppapi.map.getCenter();
+			if( ppapi.pplist.length == 0 ) ppapi.initZip(ppapi.zipcode);
+			//ppapi.reloadPP(false);
 		},
-	
+
 		setOpts: function(opts){
 			if(!opts) opts = {};
 			ppapi.opts = opts;
 			if(!ppapi.opts.group) ppapi.opts.group = '';
 		},
-	
-		displayPP: function(){
+
+		displayPP: function(fitBounds){
+			if( fitBounds == undefined ) fitBounds = true;
 			var ppselectobj = $('#pp-select-postapont');
 			ppselectobj.empty();
-	
+
 			var lat_min = 90, lat_max = -90, lon_min = 180, lon_max = -180, n = 0;
 			if(ppapi.pplist) $.each(ppapi.pplist, function(key, item) {
-				ppselectobj.append($("<option />").val(item.id).text(item.name));
+				ppselectobj.append($("<option />").val(item.id).text( item.name+' ('+item.zip+' '+item.county+', '+item.address+')' ));
 				if( n==0 || item.distance<30 ) {
 					lat_min = Math.min(lat_min, item.lat);
 					lon_min = Math.min(lon_min, item.lon);
@@ -150,8 +198,8 @@ jQuery(document).ready(function($){
 					n++;
 				}
 			});
-	
-			if( n>0 ){
+
+			if( fitBounds && n>0 ){
 				var southWest = new google.maps.LatLng(lat_min,lon_min);
 				var northEast = new google.maps.LatLng(lat_max,lon_max);
 				var bounds = new google.maps.LatLngBounds(southWest,northEast);
@@ -160,77 +208,96 @@ jQuery(document).ready(function($){
 					ppapi.map.setZoom(13);
 				} else ppapi.map.fitBounds( bounds );
 			}
+
 		},
-	
+
 		displayPPMarkers: function(){
 			for (var i = 0; i < ppapi.markers.length; i++) ppapi.markers[i].setMap(null);
 			delete(ppapi.markers);
 			ppapi.markers = [];
 			if (!ppapi.ppmarkerslist) return;
-	
-			var zIndex = 1;
+
 			$.each(ppapi.ppmarkerslist, function(key, item) {
 				if ( ppapi.skipgroups[item.group] ) return;
-	
+
 				var marker = new google.maps.Marker({
 					position: new google.maps.LatLng(item.lat, item.lon),
-					map: ppapi.map,
+					//map: ppapi.map,
 					flat: true,
 					icon: ppapi.symbols[item.group],
 					title: item.name,
 					data: ppapi.ppmarkerslist[key],
-					zIndex: zIndex
 				});
-				zIndex++;
 				ppapi.markers.push(marker);
 				marker.data.index = key;
-	
+
 				(function(marker) {
 					google.maps.event.addListener(marker, 'click', function(){ppapi.mapOpenMarkerInfo(marker);});
 				}(marker));
 			});
-			
-			//Show on load
-			ppapi.zipcode = $('#'+zipField).val();
-			ppapi.initZip(ppapi.zipcode);
-			
+
+			markerClusterer = null;
+			var mcOptions = {
+				maxZoom: 13,
+				gridSize: 50,
+				imagePath: ppapi.APIURL+'../static/img/ppapi/clusterer-m'
+			};
+			markerClusterer = new MarkerClusterer(ppapi.map, [], mcOptions);
+			for(var i in markerClusterer.styles_) markerClusterer.styles_[i].textColor = 'white';
+			markerClusterer.addMarkers(ppapi.markers, true);
 		},
-	
+
 		mapOpenMarkerInfo: function(marker){
-			ppapi.infoWindow.setContent('<div class="pp-map-info" style="width:340px; min-height:60px;">'
+			var html = '<div class="pp-map-info" style="overflow: hidden;">'
 				+'<b>' + marker.data.name + '</b><br />'
-				+'Cím: ' + marker.data.zip + ' ' + marker.data.county + ', ' + marker.data.address + '<br />'
+				+'Cím: ' + marker.data.zip + ' ' + marker.data.county + ',<br />' + marker.data.address + '<br />'
 				+ ((marker.data.phone) ?'Telefon: ' + marker.data.phone + '<br />' :'')
-				+'<a href="#" style="color:#00f;" onclick="ppapi.mapMarkerSelect(ppapi.ppmarkerslist['+marker.data.index+']); return false;">Kiválaszt</a>'
-				+'<\/div>');
+				+'<a href="#" style="color:#00f;" onclick="ppapi.mapMarkerSelect('+JSON.stringify(marker.data).replace(/\"/g,'\'')+'); return false;">Kiválaszt</a>'
+				+'<\/div>';
+			ppapi.infoWindow.setContent(html);
 			ppapi.infoWindow.setPosition(marker.position);
 			ppapi.infoWindow.open(ppapi.map);
 		},
-	
+
 		mapMarkerSelect: function(item){
 			var ppselectobj = $('#pp-select-postapont');
 			var optobj = $('option[value="' + item.id + '"]', ppselectobj);
-			if(optobj.length) optobj.attr('selected', 'selected');
-			else ppselectobj.append($("<option />").val(item.id).text(item.name).attr('selected', 'selected'));
+			if(optobj.length){
+				optobj.attr('selected', 'selected');
+			} else {
+				ppapi.pplist.push(item);
+				ppselectobj.append($("<option />").val(item.id).text( item.name+' ('+item.zip+' '+item.county+', '+item.address+')' ).attr('selected', 'selected'));
+			}
 			return ppapi.selectPP(item);
 		},
-	
+
 		selectPP: function(data){
 			ppapi.infoWindow.close();
 			if(ppapi.onSelect) ppapi.onSelect(data);
 			return false;
 		},
-	
+
+		zoomtoPP: function(data){
+			ppapi.infoWindow.close();
+			ppapi.map.panTo( new google.maps.LatLng(data.lat, data.lon) );
+			if( ppapi.map.getZoom() < 14 ) ppapi.map.setZoom(14);
+		},
+
 		setMarkers: function(layer, display){
+	//		if(layer == '20_molkut') layer = '40_postapontplusz';
 			if(display == undefined) display = true;
 			ppapi.skipgroups[layer] = (display) ?false :true;
 			ppapi.displayPPMarkers();
 			ppapi.initZip(ppapi.zipcode);
+		},
+
+		initGeolocation: function(position){
+			ppapi.zipkrd = new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+			ppapi.map.setCenter( ppapi.zipkrd );
+			ppapi.reloadPP();
 		}
-	
+
 	};
-
-
 
 	$( 'body' ).bind( 'updated_checkout', function() {
 
@@ -242,20 +309,19 @@ jQuery(document).ready(function($){
 					ppapi.setMarkers(value, false);
 				});
 			}
-		
+
 			ppapi.linkZipField(zipField); //<-- A megrendelő form input elemének a megjelölése (beállítása a kiválasztó számára)
 			ppapi.insertMap('postapontvalasztoapi'); //<-- PostaPont választó API beillesztése ( ilyen azonosítóval rendelkező DOM objektumba)
 			ppapi.onSelect = function(data){ //<-- Postapont kiválasztásra bekövetkező esemény lekötése
 				//A kiválasztott PostaPont adatainak visszaírása a megrendelő form rejtett mezőjébe.
 				$('#wc_selected_postapont').val( data['name'] + '|' +data['zip'] +'|'+ data['county'] +'|'+ data['address'] );
-			
+
 				//Adatkiírás
 				$('#valasztott_postapont').show().find('p').html(data['name'] + '<br>' +data['zip'] +'<br>'+ data['county'] +'<br>'+ data['address']);
 			}
-		
-		}
-	
-	});
 
+		}
+
+	});
 
 });
